3afd8a1b6b4c6b4b85c9a5c00252fd0b972d7543
StraGabor <100350609+StraGabor@users.noreply.github.com>
2024-05-06 11:30:09 +0200

    Login and registration code

--- app/src/main/java/com/example/vizoralejelento/MainActivity.java
+++ app/src/main/java/com/example/vizoralejelento/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.vizoralejelento;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,54 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText usrText;
+    EditText passwdText;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        usrText = findViewById(R.id.userName);
+        passwdText = findViewById(R.id.passwd);
+        auth = FirebaseAuth.getInstance();
+
+        /*EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });*/
+    }
+
+    public void login(View view) {
+        String usrName = usrText.getText().toString();
+        String password = passwdText.getText().toString();
+
+        auth.signInWithEmailAndPassword(usrName, password).addOnCompleteListener(this, task -> {
+            if(task.isSuccessful()){
+                Toast.makeText(MainActivity.this, "Sikeres bejelentkezes", Toast.LENGTH_LONG).show();
+                startDiktate();
+            } else {
+                Toast.makeText(MainActivity.this, "Sikertelen bejelentkezes: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+            }
         });
     }
+
+    private void startDiktate() {
+        Intent intent = new Intent(this,DiktateActivity.class);
+        startActivity(intent);
+    }
+
+    public void registrate(View view) {
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/vizoralejelento/Registration.java
+++ app/src/main/java/com/example/vizoralejelento/Registration.java
@@ -1,6 +1,11 @@
 package com.example.vizoralejelento;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,62 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Registration extends AppCompatActivity {
+
+    EditText user;
+    EditText email;
+    EditText passwd;
+    EditText passwdCOnfirm;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        user = findViewById(R.id.usrField);
+        email = findViewById(R.id.emailField);
+        passwd = findViewById(R.id.passwdField);
+        passwdCOnfirm = findViewById(R.id.repassField);
+        auth = FirebaseAuth.getInstance();
+
+        /*EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });*/
+    }
+
+    public void register(View view) {
+        String userName = user.getText().toString();
+        String mail = email.getText().toString();
+        String password = passwd.getText().toString();
+        String confirmPassword = passwdCOnfirm.getText().toString();
+
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(Registration.this,"Nem egyenlo a ket jelszo!",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(this, task ->{
+           if (task.isSuccessful()){
+               Toast.makeText(Registration.this,"Sikeres regisztracio!",Toast.LENGTH_LONG).show();
+               startDiktate();
+           }else{
+               Toast.makeText(Registration.this, "Sikertelen regisztracio: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+           }
         });
+    }
+
+    private void startDiktate() {
+        Intent intent = new Intent(this,DiktateActivity.class);
+        startActivity(intent);
+    }
+
+    public void cancel(View view) {
+        finish();
     }
 }
